d625771f74403d59d134fa84b44bd04b6cea91db
Daniel Northcutt <daniel.h.northcutt@gmail.com>
2022-02-08 15:19:05 -0600

    Update to functions

--- prepare.py
+++ prepare.py
@@ -1,6 +1,3 @@
-
-#### Pulled from prepare.py from NLP Exercises 
-
 import unicodedata
 import re
 import json
@@ -12,6 +9,9 @@ from nltk.corpus import stopwords
 import pandas as pd
 import acquire
 from time import strftime
+
+from sklearn.model_selection import train_test_split
+
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -112,7 +112,9 @@ def remove_stopwords(string, extra_words=[], exclude_words=[]):
 
 
 
-def prep_article_data(df, column, extra_words=[], exclude_words=[]):
+#### MAIN FUNCTION ####
+
+def prep_article_data(df, column = 'readme_contents', extra_words=[], exclude_words=[]):
     '''
     This function take in a df and the string name for a text column with 
     option to pass lists for extra_words and exclude_words and
@@ -125,18 +127,50 @@ def prep_article_data(df, column, extra_words=[], exclude_words=[]):
                                 extra_words=extra_words, 
                                 exclude_words=exclude_words)
     
-    df['stemmed'] = df[column].apply(basic_clean)\
-                            .apply(tokenize)\
-                            .apply(stem)\
-                            .apply(remove_stopwords, 
-                                extra_words=extra_words, 
-                                exclude_words=exclude_words)
-    
-    df['lemmatized'] = df[column].apply(basic_clean)\
-                            .apply(tokenize)\
-                            .apply(lemmatize)\
-                            .apply(remove_stopwords, 
-                                extra_words=extra_words, 
-                                exclude_words=exclude_words)
+    df['stemmed'] = df['clean'].apply(stem)
+    df['lemmatized'] = df['stemmed'].apply(lemmatize)
     
     return df
+
+#####
+
+def isEnglish(s):
+    '''This function filters out for just english words in readme_contents for github repos'''
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+        
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+
+####
+
+def get_top_5_languages(df):
+    '''This function filters for just the top 5 languages and drops the rest'''
+    top5_list = list(df.language.value_counts().head(5).index)
+    mask = df.language.apply(lambda x: x in top5_list)
+    df = df[mask]
+    return df
+
+###Splitzzy
+def split_data(df):
+    '''
+    Takes in a dataframe and returns train, validate, and test subset dataframes. 
+    '''
+    train, test = train_test_split(df, test_size = .2, random_state = 123)
+    train, validate = train_test_split(train, test_size = .3, random_state = 123)
+    return train, validate, test
+
+
+#### Filter Data w/ Functions and splits data into train, validate, & test
+def filter_data(df):
+    # drops nulls
+    df = df.dropna()
+    # drops repos that readme is not written in English
+    df =df[df.readme_contents.apply(isEnglish) == True]
+    # pulls top 5 languages
+    df = get_top_5_languages(df)
+    train, validate, test = split_data(df)
+
+    return train, validate, test
